de7d72c12cf477b514dd5fca8681c2f00a474496
Prasikcz <dominik.pr@seznam.cz>
2024-10-03 13:57:21 +0200

    aa

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -7,11 +7,11 @@ const Header = () => {
         <header className="bg-dark d-flex justify-content-between align-items-center px-5 py-3">
             <Link to="/" className="d-flex align-items-center text-white fw-bold"><img className="logo" src="/media/logo.png" alt="Logo" /></Link>
             <div className="toolBar text-white d-flex flex-end gap-3">
-                <Link to="/"><img src="/search.svg" alt="" title="Hledat" /></Link>
-                <Link to="/"><img src="/app.svg" alt="" title="Aplikace" /></Link>
-                <Link to="/"><img src="/settings.svg" alt="" title="Nastavení" /></Link>
-                <Link to="/"><img src="/admin.png" alt="" title="Admin" /></Link>
-                <Link to="/"><img src="/logout.png" alt="" title="Odhlásit se"/></Link>
+                <Link to="/"><img src="/media/search.svg" alt="" title="Hledat" /></Link>
+                <Link to="/"><img src="/media/app.svg" alt="" title="Aplikace" /></Link>
+                <Link to="/"><img src="/media/settings.svg" alt="" title="Nastavení" /></Link>
+                <Link to="/"><img src="/media/admin.png" alt="" title="Admin" /></Link>
+                <Link to="/"><img src="/media/logout.png" alt="" title="Odhlásit se"/></Link>
             </div>
         </header>
     );
